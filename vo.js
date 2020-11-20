4794292b07343be17aff853f88205fd84fc6b459
Nicolas De waegenaere <de_waegenaere@live.fr>
2020-11-20 19:10:02 +0100

    find word with js

--- vo.js
+++ vo.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     var str=sel.anchorNode.nodeValue,len=str.length, a=b=sel.anchorOffset;
     while(str[a]!=' '&&a--){}; if (str[a]==' ') a++; // start of word
     while(str[b]!=' '&&b++<len){};                   // end of word+1
-    console.log(str.substring(a,b));
+    englishWord = str.substring(a,b);
+    console.log(englishWord)
     });
 });
+
