80374064897b710177a540cc63b5ed6618d89bf5
Nicolas De waegenaere <de_waegenaere@live.fr>
2020-11-30 20:37:31 +0100

    reading front to back (test)

---
+++ controller/reading.crtl.php
@@ -0,0 +1,17 @@
+<?php
+	// include "reading.class.php";
+
+	// echo getText('vo.html');
+
+	// $englishWord = $_POST['englishWord'];
+	// echo($englishWord);
+	// print_r($_POST["word"]);
+
+
+	echo "Premier var dump \n";
+	var_dump($_POST);
+	echo "\n";
+	echo "Second var dump \n";
+	var_dump($_REQUEST);
+	
+?>
