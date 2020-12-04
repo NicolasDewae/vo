9fa1632861b5b2f787d57cf112242ddea2e370b8
Nicolas De waegenaere <de_waegenaere@live.fr>
2020-12-04 19:13:57 +0100

    main.view.php becomes de main file

---
+++ view/main.view.php
@@ -0,0 +1,14 @@
+<?php
+
+// everything is after "ob_start" will be contained in the variable $content with "ob_get_clean"
+ob_start();
+?>
+
+Accueil de la page
+
+<?php
+$content = ob_get_clean();
+$title = "Page d'accueil";
+require "../view/template.php";
+
+?>
