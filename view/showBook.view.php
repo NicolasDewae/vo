b5a962429f114b34ab1a4bfb77a705b9bb6b1b1c
Nicolas De waegenaere <de_waegenaere@live.fr>
2020-12-13 20:59:43 +0100

    book view started

--- view/showBook.view.php
+++ view/showBook.view.php
@@ -1,5 +1,23 @@
 <?php
+ 
+ob_start(); 
 
-echo "coucou";
+?>
+
+<div class="row">
+    <div class="col-6">
+        <img src="<?php echo URL ?>public/images/<?= $book->getImage(); ?>">
+    </div>
+    <div class="col-6">
+        <p>Titre : <?php echo $book->getTitle(); ?></p>
+        <p>Nombre de pages : <?php echo $book->getNbPages(); ?></p>
+    </div>
+</div>
+
+<?php
+
+$content = ob_get_clean();
+$title = $book->getTitle();
+require "template.php";
 
 ?>
