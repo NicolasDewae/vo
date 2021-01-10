c722ab5f5f44a02962fab109b9c8492350454ba5
Nicolas De waegenaere <de_waegenaere@live.fr>
2021-01-10 11:17:04 +0100

    inscription view

---
+++ view/inscription.view.php
@@ -0,0 +1,35 @@
+<?php 
+ob_start();
+?>
+<form method="POST" action="<?php echo URL ?>inscription" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label for="firstname" class="form-label">Prénom</label>
+        <input type="text" class="form-control" id="firstname" name="firstname">
+    </div>
+    <div class="mb-3">
+        <label for="lastname" class="form-label">Nom</label>
+        <input type="text" class="form-control" id="lastname" name="lastname">
+    </div>
+    <div class="mb-3">
+        <label for="username" class="form-label">Identifiant</label>
+        <input type="text" class="form-control" id="username" name="username">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Adresse email</label>
+        <input type="email" class="form-control" id="email" name="email"></input>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">importer une image</label>
+        <input class="form-control-file" type="password" id="password" name="password">
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">importer une image</label>
+        <input class="form-control-file" type="password" id="password_conf" name="password_conf">
+    </div>
+    <button type="submit" class="btn btn-primary">S'inscrire</button>
+</form>
+<?php
+$content = ob_get_clean();
+$title = "Inscription";
+require "template.php";
+?>
