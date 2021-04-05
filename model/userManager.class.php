a04a530fca815635c914b92f0d3b3f586e79b5cd
De waegenaere <de_waegenaere@live.fr>
2021-04-05 09:47:45 +0200

    getLogin function + try catch in userManagement

--- model/userManager.class.php
+++ model/userManager.class.php
@@ -16,32 +16,60 @@ require_once "user.class.php";
         /**
          * Insert user in database 
          */
-        public function addUserDB($firstname,$lastname,$email, $password){
-            $req = "INSERT INTO user (firstname, lastname, email, password)
-            values (:firstname, :lastname, :email, :password)";
-            $stmt = $this->getInstance()->prepare($req);
-            $stmt->bindValue(":firstname",$firstname,PDO::PARAM_STR);
-            $stmt->bindValue(":lastname",$lastname,PDO::PARAM_STR);
-            $stmt->bindValue(":email",$email,PDO::PARAM_STR);
-            $stmt->bindValue(":password",$password,PDO::PARAM_STR);
-            $resultat = $stmt->execute();
-            $stmt->closeCursor();                
+        public function addUserDB($firstname,$lastname,$email,$password,$role){
+            try {
+                $req = "INSERT INTO user (firstname, lastname, email, password, role)
+                values (:firstname, :lastname, :email, :password, :role)";
+                $stmt = $this->getInstance()->prepare($req);
+                $stmt->bindValue(":firstname",$firstname,PDO::PARAM_STR);
+                $stmt->bindValue(":lastname",$lastname,PDO::PARAM_STR);
+                $stmt->bindValue(":email",$email,PDO::PARAM_STR);
+                $stmt->bindValue(":password",$password,PDO::PARAM_STR);
+                $stmt->bindValue(":role",$role,PDO::PARAM_STR);
+                $resultat = $stmt->execute();
+                $stmt->closeCursor(); 
+            } catch (\Exception $e) {
+                echo 'Error : '.$e->getMessage();
+            }                  
         }
 
         /**
          * Verify if email exist in database
          */
         public function getEmailUser($email){
-            $req = $this->getInstance()->prepare("SELECT email FROM user WHERE email = '$email'");
-            $req->execute();
-            // avoid having duplicates
-            $userEmail = $req->fetchAll(PDO::FETCH_ASSOC);
-            $req->closeCursor();
-            
-            if ($userEmail == true) {
-                return true;
-            }else {
-                return false;
+            try {
+                $req = $this->getInstance()->prepare("SELECT email FROM user WHERE email = '$email'");
+                $req->execute();
+                $userEmail = $req->fetchAll(PDO::FETCH_ASSOC);
+                $req->closeCursor();
+                // if email exist, return true
+                if ($userEmail == true) {
+                    return true;
+                }else {
+                    return false;
+                }
+            } catch (\Exception $e) {
+                echo 'Error : '.$e->getMessage();
+            }
+        }
+
+        /**
+         * Verify if email and password exist in database
+         */
+        public function getlogin($emailConnect, $passwordConnect){
+            try {
+                $req = $this->getInstance()->prepare("SELECT * FROM user WHERE email = '$emailConnect' AND password = '$passwordConnect'");
+                $req->execute();
+                $login = $req->fetchAll(PDO::FETCH_ASSOC);
+                $req->closeCursor();
+                // if email ans password exist, return true
+                if ($login == true) {
+                    return true;
+                }else {
+                    return false;
+                }
+            } catch (\Exception $e) {
+                echo 'Error : '.$e->getMessage();
             }
         }
     }
