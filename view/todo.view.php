9aaf2d4a6cf8e891e33ffa965f91e922c80b5e37
De waegenaere <de_waegenaere@live.fr>
2021-04-10 12:55:09 +0200

    todo updated

--- view/todo.view.php
+++ view/todo.view.php
@@ -3,12 +3,18 @@ ob_start();
 ?>
 <h2>Mises à jours principales</h2>
 <ol>
-    <li>Verifier les acces des users (connectés) et des visiteurs (non connectés)</li>
     <li>Implémenter du JS pour dynamiser l'application pour:</li>
     <ul>
         <li>Ajouter des alertes de succès et/ou d'erreurs</li>
         <li>Personnaliser les alertes lors des suppressions de livres </li>
     </ul>
+    <li>Donner la possibilité de trier ses livres en sous catégories à l'aide de Navs bootstrap:</li>
+    <ul>
+        <li>J'aime</li>
+        <li>Je n'aime pas</li>
+    </ul>
+    <li>Ajouter un champ "note" dans showBook.view.php</li>
+
 </ol>
 
 <h2>Mises à jours secondaires:</h2>
@@ -16,13 +22,6 @@ ob_start();
     <li>Ajouter une page d'accueil grand publique avec explication de l'application</li>
     <li>Ajouter une page d'accueil pour les recruteurs avec lien LinkedIn, github et explication technique du projet</li>
     <li>Réaliser l'ajout des livres via une bibliotheque appelée à l'aide d'une API</li>
-    <li>Donner la possibilité de trier ses livres en sous catégories à l'aide de Navs bootstrap:</li>
-        <ul>
-            <li>J'ai</li>
-            <li>Je veux</li>
-            <li>Je souhaite</li>
-            <li>J'échange (Dans le cas futur ou l'on met en place un système de profil public avec un messagerie)</li>
-        </ul>
     <li>Ajouter de la lecture de texte anglophone avec traduction des mots au clic</li>
 </ol>
 
