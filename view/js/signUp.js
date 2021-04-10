214cc0a29929e74d692d5276b4745492a466f8ab
De waegenaere <de_waegenaere@live.fr>
2021-04-10 12:55:42 +0200

    vo

---
+++ view/js/signUp.js
@@ -0,0 +1,45 @@
+    // $("#loginForm").click(function(){
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "controller/user.crtl.php",
+    //         data: {
+    //             email: $('select[name=emailConnect]').val(),
+    //             password: $('input[name=passwordConnect]').val(),
+    //         },
+    //         dataType:'JSON', 
+    //         success: function(response){
+    //             console.log(response.error);
+    //             // put on console what server sent back...
+    //         },
+    //         error : function(resultat, statut, erreur){
+    //             console.log(data);
+    //         }
+    //     }); 
+    // });
+function msgRetour() {
+    alert('ok');
+    // $('#updateBook').submit(function(e) {
+    //     e.preventDefault();
+    //     // $('.comments').empty();
+    //     var postdata = $('#loginForm').serialize();
+        
+    //     $.ajax({
+    //         type: 'POST',
+    //         url: '../controller/book.crtl.php',
+    //         data: postdata,
+    //         dataType: 'json',
+    //         success: function(json) {
+                 
+    //             if(json.login == true) 
+    //             {
+    //                 alert("connection ok");
+    //             }
+    //             else
+    //             {
+    //                 alert("connection pas ok");
+    //             }                
+    //         }
+    //     });
+    // });
+   
+}
