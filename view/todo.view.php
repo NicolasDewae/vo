b3727c3cbbce89d24b2f5d966f38e1eeb0f057ae
De waegenaere <de_waegenaere@live.fr>
2021-04-16 10:48:00 +0200

    todo updated

--- view/todo.view.php
+++ view/todo.view.php
@@ -5,22 +5,20 @@ ob_start();
 <ol>
     <li>Implémenter du JS pour dynamiser l'application pour:</li>
     <ul>
-        <li>Ajouter des alertes de succès et/ou d'erreurs</li>
-        <li>Personnaliser les alertes lors des suppressions de livres </li>
+        <li>Ajouter des alertes de succès et d'erreurs</li>
     </ul>
     <li>Donner la possibilité de trier ses livres en sous catégories à l'aide de Navs bootstrap:</li>
     <ul>
         <li>J'aime</li>
         <li>Je n'aime pas</li>
     </ul>
-    <li>Ajouter un champ "note" dans showBook.view.php</li>
+    <li>Ajouter un champ "note" dans showBook.view.php et updateBook.view.php</li>
 
 </ol>
 
 <h2>Mises à jours secondaires:</h2>
 <ol>
-    <li>Ajouter une page d'accueil grand publique avec explication de l'application</li>
-    <li>Ajouter une page d'accueil pour les recruteurs avec lien LinkedIn, github et explication technique du projet</li>
+    <li>Ajouter une page d'accueil avec lien LinkedIn, github et explication technique du projet</li>
     <li>Réaliser l'ajout des livres via une bibliotheque appelée à l'aide d'une API</li>
     <li>Ajouter de la lecture de texte anglophone avec traduction des mots au clic</li>
 </ol>
